665a0d576b68dbcc6e28aff699bc861cf7cf28ae
dinidusachintha <dinidusachintha7@gmail.com>
2025-08-10 22:54:31 +0530

    nav logo link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import assets from '../assets'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import assets from '../assets';
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <div>
-      
-      <img src={assets.logo} alt="logo" className="h-8"/>
-    </div>
-  )
-}
+      <Link to="/">
+        <img src={assets.logo} alt="Logo" className="h-8" />
+      </Link>
 
-export default Navbar
+      <div>
+        {menuLinks.map((link,index) => (
+          <link. key={index}> to={link.path} className="text-gray-700 hover:text-blue-500">
+            {link.name}
+          </link>
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
