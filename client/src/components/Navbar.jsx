466b92fc437944de63da8b45e1ecffa271a3b902
dinidusachintha <dinidusachintha7@gmail.com>
2025-08-10 22:34:19 +0530

    add browser router in main

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import assets from '../assets'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div>
+      
+      <img src={assets.logo} alt="logo" className="h-8"/>
+    </div>
+  )
+}
+
+export default Navbar
